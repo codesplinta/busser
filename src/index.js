e37082667b573bc9f0fcc5b6bd15faec6c0a4aa3
CodeSplintaNG <76159782+codesplinta@users.noreply.github.com>
2021-06-25 17:49:22 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import { EventBusProvider, useEventBus } from './eventbus/core'
+import { useUIStateManager } from './managers/ui'
+import { useUIDataFetcher } from './fetchers/ui'
 
+const entry = {
+  EventBusProvider, 
+  useEventBus,
+  useUIStateManager,
+  useUIDataFetcher
+}
+
+export default entry
