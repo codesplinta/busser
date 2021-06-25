20177de1be1585049e79258a7d5e3f2ea2181b5e
CodeSplintaNG <76159782+codesplinta@users.noreply.github.com>
2021-06-25 20:01:59 +0100

    Update manager.js

--- src/ui/manager.js
+++ src/ui/manager.js
@@ -1,6 +1,16 @@
 import { useState } from 'react'
 import { useEventBus } from '../eventbus/core'
 
+function isEmpty(value) {
+  return (
+      value === null || // check for null
+      value === undefined || // check for undefined
+      value === '' || // check for empty string
+      (Array.isArray(value) && value.length === 0) || // check for empty array
+      (typeof value === 'object' && Object.keys(value).length === 0) // check for empty object
+  );
+}
+
 export const useUIStateManager = ({ state = {}, events = [], updater = () => ({}) }) => {
   const allSubscribedEvents = ['request:started', 'request:ended', 'request:aborted'].concat(events)
 
