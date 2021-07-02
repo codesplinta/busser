90dde452ac1b5673a311197787890b8afff850a3
CodeSplintaNG <76159782+codesplinta@users.noreply.github.com>
2021-07-02 19:50:05 +0100

    Update fetcher.js

--- src/ui/fetcher.js
+++ src/ui/fetcher.js
@@ -1,53 +1,76 @@
-import { useState } from 'react'
+'use strict'
 import { useEventBus } from '../eventbus/core'
-import { getHttpClientDriver } from '../helpers'
+import { getHttpClientDriver, getDefaultHttpServerUrl } from '../helpers'
 
 export function useUIDataFetcher ({ 
-  httpClientDriverName = 'axios',
-  defaultFetchData = null,
-  defaultFetchError = null,
-  customizePayload = r => {
-    return r;
+  customizePayload = response => {
+    return response;
   }
  }) {
   const bus = useEventBus([], ['request:started', 'request:ended', 'request:aborted', 'cleanup'])
 
-  const [fetchData, setFetchData] = useState(defaultFetchData);
-  const [fetchError, setFetchError] = useState(defaultFetchError);
-  Const fetch = getHttpClientDriver(httpClientDriverName)
-  const _fetch = (urlOrQuery, params = {}, method = 'POST') => {
-    bus.emit('request:started', {})
+  const { httpClientDriverName, fetch } = getHttpClientDriver()
+  const _fetch = (url, params = {}, method = 'POST', metadata = {}) => {
+      const asQuery = params.query.search(/^\s*query\s{1,}\{/i) !== -1
+      const asMutation = params.query.search(/^\s*mutation\s{1,}\{/i) !== -1
+      const asSubscription = params.query.search(/^\s*subscription\s{1,}\{/i) !== -1
+
+      if (method.toLowerCase() === 'get'
+	|| method.toLowerCase() === 'post') {
+    	if (typeof params.query === 'string'
+	    && (asQuery || asMutation)) {
+	  metadata.isGraphQl = true;
+          metadata.isQueryType = asQuery && 'query' || asMutation && 'mutation' || asSubscription && 'subscription'
+	} else {
+	  metadata.isGraphQl = false;
+	}
+      } else if (method.toLowerCase() !== 'get'
+	|| method.toLowerCase() !== 'head') {
+    	metadata.isGraphQl = false;
+      }
+
+      bus.emit('request:started', { success: true, error: null, metatdata })
+
       let promise = null
+      let url = getDefaultHttpServerUrl(url)
+
       if (httpClientDriverName === 'axios') {
         promise = fetch()
       } else if (httpClientDriverName === 'isomorphic-fetch') {
-        promise = fetch(urlOrQuery, params)
+        promise = fetch(url, params)
       } else if (httpClientDriverName === 'fetch') {
-	promise = fetch(urlOrQuery, params)
+	promise = fetch(url, params)
       }
+
       return promise.then(function onData(payload) {
         if (payload.error) {
-          bus.emit('request:ended', { error: payload.error, success: null });
+          bus.emit('request:ended', {
+	    error: customizePayload(
+	      payload instanceof Error ? payload : payload.error || payload,
+	      'error'
+            ),
+	    success: null,
+	    metadata
+    	  });
         } else {
           bus.emit('request:ended', {
-            success: payload.response,
+            success: customizePayload(
+	      payload.response || payload,
+	      'response'
+	    ),
             error: null,
-            metadata: { customizePayload }
+            metadata
           });
         }
-        setFetchData(customizePayload(payload.response, 'response'));
       })
       .catch(function onError(payload) {
-        setFetchError(
-          customizePayload(
-            payload instanceof Error ? payload : payload.error,
-            'error'
-          )
-        );
         bus.emit('request:ended', {
           success: null,
-          error: payload instanceof Error ? payload : payload.error,
-          metadata: { customizePayload }
+          error: customizePayload(
+            payload instanceof Error ? payload : payload.error || payload,
+            'error'
+          ),
+          metadata
         });
       })
       .finally(function onFinish() {
@@ -61,27 +84,9 @@ export function useUIDataFetcher ({
       : templateFunction;
   };
 
-  const connectToReporter = (templateFunction, isErrorReporter = false) => {
-    return typeof templateFunction === 'function'
-      ? templateFunction.bind(
-          null,
-          isErrorReporter ? setFetchError : setFetchData
-        )
-      : templateFunction;
-  };
-
-  const connectToFetcherAndReporter = templateFunction => {
-    return typeof templateFunction === 'function'
-      ? connectToReporter(connectToFetcher(templateFunction), true)
-      : templateFunction;
-  };
-
   return {
-    connectToFetcherAndReporter,
     connectToFetcher,
     fetcher:_fetch,
-    fetchData,
-    fetchError
   };
 };
 
