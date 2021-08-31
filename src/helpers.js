7f0590d7ba355820a428b90b7bf69a9ed6a94e34
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2021-08-31 09:13:19 +0100

    Update helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -1,11 +1,23 @@
 'use strict';
 
-const getHttpClientDriver = () => {
-  return { httpClientDriverName, fetch }
+const getHttpClientDriverName = (httpClientDriver) => {
+  if (!httpClientDriver || typeof httpClientDriver !== 'function') {
+    return 'unknown'
+  }
+
+  const stringifiedDriver = httpClientDriver.toString()
+
+  if (httpClientDriver.name === 'fetch' 
+      && stringifiedDriver.startsWith('function fetch() {\n')
+        && stringifiedDriver.endsWith('[native code]\n}')) {
+    return 'fetch'
+  }
+
+  if (httpClientDriver.name === 'wrap'
+      && typeof httpClientDriver.Axios === 'function') {
+    return 'axios'
+  }
 }
 
-const getDefaultHttpServerUrl = (url) => {
-  return url || ''
-}
 
-export { getHttpClientDriver, getDefaultHttpServerUrl }
+export { getHttpClientDriverName  }
