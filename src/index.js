ee74d0490427fd6c3ee6758abbbffd2f6e9cd4dd
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2022-05-09 16:29:17 +0100

    chore: additional hooks

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import { EventBusProvider, useEventBus, useEventListener } from './eventbus/core'
+import { EventBusProvider, useBus, useOn, useWhen, useUpon, usePageRouted } from './eventbus/core'
 import { useUIStateManager } from './ui/manager'
 import { HttpClientProvider, useUIDataFetcher, useFetchBinder } from './ui/fetcher'
 
 const entry = {
   EventBusProvider,
   HttpClientProvider,
-  useEventBus,
-  useEventListener,
+  useBus,
+  useuseOn,
+  useWhen,
+  useUpon,
+  usePageRouted,
   useUIStateManager,
   useUIDataFetcher,
   useFetchBinder
