36e42f9229f2dfcb4c899eac40953a896c998e07
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2022-07-03 09:42:05 +0100

    chore: additional hooks

--- src/eventbus/core.jsx
+++ src/eventbus/core.jsx
@@ -6,8 +6,132 @@ import React, {
   useCallback,
   useRef
 } from "react";
+import { toUniqueItemList, extractPropertyValue } from "../helpers";
 
+const BrowserStorageContext = React.createContext(null);
+const TextFilterAlgorithmsContext = React.createContext(null);
 const EventBusContext = React.createContext(null);
+
+const TextFilterAlgorithmsProvider = ({
+  children,
+  extendAlgos = {}
+}) => {
+  const shared = useRef(
+    Object.assign(extendAlgos, {
+      specific(filterText = "", filterList = [], filterListItemKey = "") {
+        return filterList.filter((filterListItem = "") => {
+          const listItem =
+            typeof filterListItem !== "object"
+              ? filterListItem
+              : extractPropertyValue(filterListItemKey, filterListItem);
+          const haystack =
+            typeof listItem === "string" ? listItem.toLowerCase() : listItem;
+          const needle = filterText.toLowerCase();
+
+          return (filterText === "" || haystack.indexOf(needle) > -1);
+        });
+      },
+      fuzzy(filterText = "", filterList = [], filterListItemKey = "") {
+        if (filterText === "") {
+          return filterList;
+        }
+
+        const characters = filterText.split("");
+
+        /* @NOTE: flatten the multi-dimesional filtered list (array) */
+        const chunks = [].concat.apply(
+          [],
+          characters.map((character) => {
+            return filterList.filter((filterListItem) => {
+              const needle = character.toLowerCase();
+              const listItem =
+                typeof filterListItem !== "object"
+                  ? filterListItem
+                  : extractPropertyValue(filterListItemKey, filterListItem);
+              const haystack =
+                typeof listItem === "string"
+                  ? listItem.toLowerCase()
+                  : listItem;
+              let radix = haystack.indexOf(needle);
+
+              if (radix === -1) {
+                return false;
+              }
+              return true;
+            });
+          })
+        );
+
+        return toUniqueItemList(chunks, filterListItemKey);
+      },
+      complete(filterText = "", filterList = [], filterListItemKey = "") {
+        return filterList.filter((filterListItem) => {
+          const listItem =
+            typeof filterListItem !== "object"
+              ? filterListItem
+              : extractPropertyValue(filterListItemKey, filterListItem);
+          const haystack =
+            typeof listItem === "string" ? listItem.toLowerCase() : listItem;
+          const needle = filterText.toLowerCase();
+
+          let radix = -1,
+            charPosition = 0,
+            charValue = needle[charPosition] || null;
+
+          while (null !== charValue) {
+            radix = haystack.indexOf(charValue, radix + 1);
+            if (!~radix) {
+              return false;
+            }
+            charPosition += 1;
+            charValue = needle[charPosition] || null;
+          }
+          return true;
+        });
+      }
+    })
+  );
+
+  return (
+    <TextFilterAlgorithmsContext.Provider value={shared.current}>
+      {children}
+    </TextFilterAlgorithmsContext.Provider>
+  );
+};
+
+const BrowserStorageProvider = ({ children, storageDriver = {} }) => {
+  const shared = useRef({
+    setToStorage: (key, value) => {
+      /* @HINT: This is the side-effect for each state change cycle - we want to write to `sessionStoage` */
+      if (typeof storageDriver.setItem === "function") {
+        storageDriver.setItem(key, JSON.stringify(value));
+      }
+    },
+    clearFromStorage: (key) => {
+      /* @HINT: As the component unmounts, we want to delete from `sessionStorage` */
+      if (typeof storageDriver.removeItem === "function") {
+        storageDriver.removeItem(key);
+      }
+    },
+    getFromStorage: (key, defaultPayload) => {
+      /* @HINT: We want to fetch from `sessionStorage` */
+      let stringifiedPayload = "";
+
+      if (typeof storageDriver.getItem === "function") {
+        stringifiedPayload = storageDriver.getItem(key);
+      }
+
+      return !stringifiedPayload
+        ? defaultPayload
+        : JSON.parse(stringifiedPayload);
+    }
+  });
+  return (
+    <BrowserStorageContext.Provider value={shared.current}>
+      {children}
+    </BrowserStorageContext.Provider>
+  );
+};
 
 function EventBusProvider({ children }) {
   const [handlers] = useState(() => ({}));
@@ -205,13 +329,14 @@ const useList = (
   name = "<no name>"
 ) => {
   const [list, setList] = useState(initial);
+  const handleMutationTrigger = useCallback((event, listItem) => {
+    setList((prevList) => {
+      return listReducer(prevList, listItem, event);
+    });
+  }, [listReducer]);
   const [bus, stats] = useOn(
     eventsList,
-    (event, listItem) => {
-      setList((prevList) => {
-        return listReducer(prevList, listItem, event);
-      });
-    },
+    handleMutationTrigger,
     name
   );
 
@@ -226,7 +351,7 @@ const useList = (
 const useCount = (
   eventsList = [],
   countReducer,
-  { start = 0, min = 0, max = Number.MAX_SAFE_INTEGER },
+  { start = 0, min = Number.MIN_SAFE_INTEGER, max = Number.MAX_SAFE_INTEGER },
   name = "<no name>"
 ) => {
   if (
@@ -234,28 +359,29 @@ const useCount = (
     typeof min !== "number" ||
     typeof max !== "number"
   ) {
-    throw new Error("incorrect count bounds data type");
+    throw new Error("[react-busser]: incorrect count bounds data type");
   }
 
   if (start < min || start > max) {
-    throw new Error("incorrect count bounds range");
+    throw new Error("[react-busser]: incorrect count bounds range");
   }
 
   const bounds = useRef({ min, max });
   const [count, setCount] = useState(start);
+  const handleMutationTrigger = useCallback((event, directionOrCountItem) => {
+    setCount((prevCount) => {
+      const probableNextCount = prevCount + 1;
+      const probablePrevCount = prevCount - 1;
+      const limit = bounds.current;
+
+      return probablePrevCount < limit.min && probableNextCount > limit.max
+        ? prevCount
+        : countReducer(prevCount, directionOrCountItem, event);
+    });
+  }, [countReducer]);
   const [bus, stats] = useOn(
     eventsList,
-    (event, directionOrCountItem) => {
-      setCount((prevCount) => {
-        const probableNextCount = prevCount + 1;
-        const probablePrevCount = prevCount - 1;
-        const limit = bounds.current;
-
-        return probablePrevCount < limit.min && probableNextCount > limit.max
-          ? prevCount
-          : countReducer(prevCount, directionOrCountItem, event);
-      });
-    },
+    handleMutationTrigger,
     name
   );
 
@@ -267,13 +393,62 @@ const useCount = (
   ];
 };
 
+const useTextFilteredList = (
+  eventsList = [],
+  initial = [],
+  { text = "", filterTaskName = "specific" },
+  name = "<no name>",
+) => {
+  const algorithms = useContext(TextFilterAlgorithmsContext);
+  const filterTextAlgorithm = algorithms[filterTaskName];
+
+  const [controller, setController] = useState({ text, list: initial });
+
+  const handleFilterTrigger = useCallback(
+    (event, listItemKey = "") => {
+      if (
+        event &&
+        event.type === "change" &&
+        event.target instanceof Element &&
+        !event.defaultPrevented
+      ) {
+        const searchTerm = event.target.value;
+        setController((prevController) => ({
+          text: searchTerm,
+          list: controllerReducer(prevController, filterTextAlgorithm.apply(null, [searchTerm, initial, listItemKey]), "")
+        }));
+      }
+    },
+    [filterTextAlgorithm, initial]
+  );
+
+  const [bus, stats] = useOn(
+    eventsList,
+    handleFilterTrigger,
+    name
+  );
+
+  return [
+    controller,
+    handleFilterTrigger
+  ];
+};
+
+const useBrowserStorage = () => {
+  return useContext(BrowserStorageContext);
+};
+
+
 export {
   EventBusProvider,
+  BrowserStorageProvider,
+  TextFilterAlgorithmsProvider,
   useUpon,
   useWhen,
   useThen,
   useBus,
   useOn,
+  useBrowserStorage,
   useRouted,
   useList,
   useCount
