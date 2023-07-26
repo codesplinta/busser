09937bd86de6295cb5aa3073a7533a85c2638bd5
CodeSplintaNG <76159782+codesplinta@users.noreply.github.com>
2023-07-26 02:22:36 +0100

    Create stubs.js

---
+++ test/.helpers/test-doubles/stubs.js
@@ -0,0 +1,15 @@
+export const stubBasicCallback = jest.fn(() => undefined);
+export const stubEffectsCallback = jest.fn(() => () => undefined);
+
+export const stubDialogProcessFactory = (type: "alert" | "confirm" | "prompt", returnType: boolean | void) => (function () {
+
+    const prompts = {
+      alert: function () {
+        return;
+      },
+      prompt: jest.fn().mockImplementation(() => returnType),
+      confirm: jest.fn().mockImplementation(() => returnType)
+    };
+  
+    return prompts[type];
+}());
