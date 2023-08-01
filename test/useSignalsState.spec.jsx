82da36872211e45a61a524dc131fce5e2044d337
CodeSplintaNG <76159782+codesplinta@users.noreply.github.com>
2023-08-01 15:41:54 +0100

    Create useSignalsState.spec.jsx

---
+++ test/useSignalsState.spec.jsx
@@ -0,0 +1,32 @@
+// import { EventBusProvider } from '../src';
+
+import "@testing-library/react-hooks/lib/dom/pure";
+import React from "react";
+import { renderHook, act } from "@testing-library/react-hooks";
+
+import { useSignalsState } from "../src";
+
+describe("Testing `useSignalsState` ReactJS hook", () => {
+  test("should render `useSignalsState` hook and check state updates accordingly", () => {
+    const initialCountValue = 0;
+    const nextCountValue = 2;
+
+    const { result } = renderHook(() => useSignalsState(initialCountValue));
+
+    const [ count, setCount ] = result.current;
+
+    expect(count).toBeDefined();
+    expect(typeof count.value).toBe("number");
+
+    expect(setCount).toBeDefined();
+    expect(typeof setCount).toBe("function");
+
+    act(() => {
+      setCount((previousCountValue) => {
+        return previousCountValue + 2;
+      });
+    });
+
+    expect(count.value).toBe(nextCountValue);
+  });
+});
