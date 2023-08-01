fbbc37772ded38ed937096595c5f64c3d0d3bcd3
CodeSplintaNG <76159782+codesplinta@users.noreply.github.com>
2023-08-01 19:17:21 +0100

    Update stubs.js

--- test/.helpers/test-doubles/stubs.js
+++ test/.helpers/test-doubles/stubs.js
@@ -1,3 +1,7 @@
+export const stubListReducer = jest.fn((previousList, eventPayload) => {
+  return previousList.concat([ eventPayload * 2 ]);
+});
+
 export const stubBasicCallback = jest.fn(() => undefined);
 export const stubEffectsCallback = jest.fn(() => () => undefined);
 
