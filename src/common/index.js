10c1c2e190babee49205e1593f7df17ccef4fe0d
Ifeora Okechukwu <isocroft@users.noreply.github.com>
2023-10-05 01:35:35 +0100

    Update index.js

--- src/common/index.js
+++ src/common/index.js
@@ -1,16 +1,17 @@
 import { useCallback, useEffect } from "react";
-import ReactStatePrimitiveBindings from "@preact/signals-react";
+import { signal, effect, useSignal, useComputed } from "@preact/signals-react";
 
 /**
  * @SEE: https://github.com/preactjs/signals/issues/307
  */
-// function useSignal(value) {
-//   const $signal = useRef();
-//   return $signal.current ??= ReactStatePrimitiveBindings.signal(value);
-// }
+function useSignal$(value) {
+  const $signal = useRef();
+  return ($signal.current ??= signal(value));
+}
 
 export const useSignalsState = (initialState) => {
-  const signal = ReactStatePrimitiveBindings.useSignal(
+  const useSignal_ = Boolean(useSignal) ? useSignal : useSignal$;
+  const signal = useSignal_(
     typeof initialState === "function" ? initialState() : initialState
   );
 
@@ -28,7 +29,31 @@ export const useSignalsEffect = (callback = (() => undefined), depenencyList = [
   const $callback = useCallback(callback, depenencyList);
 
   useEffect(() => {
-    return ReactStatePrimitiveBindings.effect($callback);
+    return effect(($callback);
   /* eslint-disable-next-line react-hooks/exhaustive-deps */
   }, []);
 };
+
+export const useAppState = (appState, useSignals = false) => {
+  const statePrimitiveMap = {
+    signals: useSignalsState,
+    container: useState
+  };
+
+  return statePrimitiveMap[useSignals ? "signals" : "container"](
+    appState
+  );
+};
+
+export const useAppEffect = (effectCallback, useSignals = false) => {
+  const statePrimitiveMap = {
+    signals: useSignalsEffect,
+    container: useEffect
+  };
+
+  return statePrimitiveMap[useSignals ? "signals" : "container"](
+    effectCallback
+  );
+};
+
+export const useSignalsComputed = useComputed;
